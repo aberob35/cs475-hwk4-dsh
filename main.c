8314c97f0f132fcb78ee620dc0cd0a2ec2fb53d5
aberob35 <arobin@pugetsound.edu>
2023-02-20 16:34:08 -0800

    Update main.c

--- main.c
+++ main.c
@@ -11,10 +11,53 @@
 #include <sys/stat.h>
 #include <string.h>
 #include "dsh.h"
+#include <sys/wait.h>
 
-int main(int argc, char **argv)
-{
-	char cmdline[MAXBUF]; // stores user input from commmand line
+int main(int argc, char **argv){
+	//create variables 
+	char temp[256];
+	int boolean = 0;
 
+	//while loop to reprompt
+	while(boolean == 0){
+	//prompt user 1st time and adds terminating char at end of string
+	printf("\n Dsh> ");
+	fgets(temp,256,stdin);
+	temp[strlen(temp) - 1] = '\0';
+
+	//ends while loop is exit inputted
+	if(strcmp(temp,"exit") == 0){
+		boolean = 1;
+	}else if(strcmp(temp,"\0") == 0){
+	printf("ERROR EMPTY STRING ENTERED \n");
+	printf("\n Dsh> ");
+	fgets(temp,256,stdin);
+	temp[strlen(temp) - 1] = '\0';
+	}
+
+
+	if(temp[0] == '/'){
+		//checks for & symbol at end of command and runs mode 1
+		//check in order to see if it should run for or background
+		int bgVal = 0;
+		char *andCheck = strchr(temp,'&');
+		if(andCheck != NULL && andCheck == (temp + strlen(temp) -1)){
+			temp[strlen(temp) - 2] = '\0';
+			bgVal = 1;
+		}
+		mode1(temp, bgVal);
+	}else {
+		//checks for & symbol at end of command and runs mode 2
+		//check in order to see if it should run for or background
+		
+		int bgVal = 0;
+		char *andCheck = strchr(temp,'&');
+		if(andCheck != NULL && andCheck == (temp + strlen(temp) -1)){
+			temp[strlen(temp) - 2] = '\0';
+			bgVal = 1;
+		}
+		mode2(temp,bgVal);
+	}
+}
 	return 0;
 }
