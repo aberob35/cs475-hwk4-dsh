716f109fbd62267864c60ec2a69d584583cf003a
aberob35 <arobin@pugetsound.edu>
2023-02-20 16:33:22 -0800

    Update dsh.h

--- dsh.h
+++ dsh.h
@@ -9,3 +9,8 @@
 #define HISTORY_LEN 100
 
 // TODO: Your function prototypes below
+char** split(char *str, char *delim);
+void mode1(char *temp, int bgVal);
+void mode2(char *temp, int bgVal);
+void cdCommand(char *temp);
+void errorCase(char **terms);
